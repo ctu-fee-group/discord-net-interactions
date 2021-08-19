fb1b9f21126be5b6af885e7eb2ba6e48b26d9249
František Boháček <fandabohacek@gmail.com>
2021-08-19 20:43:54 +0200

    chore: rename command handler creator generic arguments to better describe purpose

--- src/Discord.Net.Interactions.Abstractions/ICommandHandlerCreator.cs
+++ src/Discord.Net.Interactions.Abstractions/ICommandHandlerCreator.cs
@@ -12,18 +12,19 @@ namespace Discord.Net.Interactions.Abstractions
     public delegate Task CommandDelegate<in T1, in T2, in T3>(SocketSlashCommand command, T1 arg1, T2 arg2, T3 arg3, CancellationToken token);
     public delegate Task CommandDelegate<in T1, in T2, in T3, in T4>(SocketSlashCommand command, T1 arg1, T2 arg2, T3 arg3, T4 arg4, CancellationToken token);
     public delegate Task CommandDelegate<in T1, in T2, in T3, in T4, T5>(SocketSlashCommand command, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, CancellationToken token);
-    
+
     /// <summary>
     /// Creator of SlashCommandHandler different types may be used for subcommands or custom matching
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface ICommandHandlerCreator<T, U>
+    /// <typeparam name="TMatcherType">Type that will be passed to the matcher as argument</typeparam>
+    /// <typeparam name="TDelegateType">Type of the delegate that will be called as the handler</typeparam>
+    public interface ICommandHandlerCreator<TMatcherType, TDelegateType>
     {
         /// <summary>
         /// Creates SlashCommandHandler for given matches
         /// </summary>
         /// <param name="matchers">List of matchers that specify what function is matched given conditions</param>
         /// <returns></returns>
-        public SlashCommandHandler CreateHandlerForCommand(IEnumerable<(Func<T, bool>, U)> matchers);
+        public SlashCommandHandler CreateHandlerForCommand(IEnumerable<(Func<TMatcherType, bool>, TDelegateType)> matchers);
     }
 }
