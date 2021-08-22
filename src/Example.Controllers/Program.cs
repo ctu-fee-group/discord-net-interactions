49b4627bee9e361235f9527a58fe3438248956bf
František Boháček <fandabohacek@gmail.com>
2021-08-22 19:07:22 +0200

    chore: add controller project

---
+++ src/Example.Controllers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord.Net.Interactions.Example.Controllers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
