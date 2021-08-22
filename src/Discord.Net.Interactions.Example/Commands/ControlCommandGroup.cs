49661aea358a530f1e5804d8811926be2e78d3c2
František Boháček <fandabohacek@gmail.com>
2021-08-22 19:06:21 +0200

    feat(example): add confirm choices for /quit

--- src/Discord.Net.Interactions.Example/Commands/ControlCommandGroup.cs
+++ src/Discord.Net.Interactions.Example/Commands/ControlCommandGroup.cs
@@ -1,8 +1,11 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Discord.Net.Interactions.Abstractions;
+using Discord.Net.Interactions.Commands;
 using Discord.Net.Interactions.CommandsInfo;
+using Discord.Net.Interactions.Components;
 using Discord.Net.Interactions.Executors;
+using Discord.Rest;
 using Discord.WebSocket;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -14,20 +17,42 @@ namespace Discord.Net.Interactions.Example.Commands
         private readonly CommandsOptions _options;
         private readonly ILogger _logger;
         private readonly StopBot _stopBot;
-        
-        public ControlCommandGroup(ILogger<ControlCommandGroup> logger, StopBot stopBot, IOptions<CommandsOptions> options)
+        private readonly IComponentHelper _componentHelper;
+
+        public ControlCommandGroup(ILogger<ControlCommandGroup> logger, StopBot stopBot,
+            IOptions<CommandsOptions> options, IComponentHelper componentHelper)
         {
+            _componentHelper = componentHelper;
             _logger = logger;
             _options = options.Value;
             _stopBot = stopBot;
         }
 
-        private Task HandleQuit(SocketInteraction interaction, CancellationToken cancellationToken)
+        private async Task HandleQuit(SocketInteraction interaction, CancellationToken cancellationToken)
+        {
+            IMessage message = await interaction.FollowupChoiceAsync("Are you sure?", ephemeral: true,
+                choices: new[] { ("Yes", "yes"), ("No", "no") });
+
+            _componentHelper.ForMessageChoice(message, interaction.User,
+                builder => builder
+                    .WithLogger(_logger)
+                    .WithThreadPool()
+                    .WithDeferMessage(),
+                ("yes", HandleConfirmQuit),
+                ("no", HandleRejectQuit));
+        }
+
+        private Task HandleConfirmQuit(SocketInteraction interaction, CancellationToken cancellationToken)
         {
             _stopBot.Invoke();
-            return interaction.FollowupAsync("Goodbye");
+            return interaction.FollowupAsync("Quitting!", ephemeral: true);
         }
-        
+
+        private Task HandleRejectQuit(SocketInteraction interaction, CancellationToken cancellationToken)
+        {
+            return interaction.FollowupAsync("Aborting!", ephemeral: true);
+        }
+
         public Task SetupCommandsAsync(IInteractionHolder holder, CancellationToken token = new CancellationToken())
         {
             // Create quit command info
@@ -38,7 +63,7 @@ namespace Discord.Net.Interactions.Example.Commands
                     .WithDescription("Exit the bot application"))
                 .WithGuild(_options.GuildId)
                 .Build();
-            
+
             // Create executor along with Deferring ephemerally and logging in case of an error
             IInteractionExecutor executor = new InteractionExecutorBuilder()
                 .WithLogger(_logger)
